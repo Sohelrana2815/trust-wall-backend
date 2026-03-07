95be4037ddb293ea4a1f93bf76358096795b0e80
Sohelrana2815 <sohel152302@gmail.com>
2026-03-07 07:05:41 +0600

    done

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,11 @@ const PORT = 5000;
 
 app.get("/", (req: Request, res: Response) => {
   console.log("Hello with pnpm and Typescript!");
+  res.send("Trust Wall Server is Running!");
+});
+
+app.get("/api", (req: Request, res: Response) => {
+  res.send("I can do this all day💪");
 });
 
 app.listen(PORT, () => {
